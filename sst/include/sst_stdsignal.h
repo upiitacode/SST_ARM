58c3342604539a6951d537d83c2c19ae9b982a8b
Manuel Limones <limonesu.me@gmail.com>
2015-01-16 23:46:39 -0600

    Create sst_stdsignal.h file to provide a set of system reserved signals

---
+++ sst/include/sst_stdsignal.h
@@ -0,0 +1,17 @@
+/*file: sst_stdsignal.h*/
+/***********************/
+#ifndef _SST_STDSIGNAL_H_
+#define _SST_STDSIGNAL_H_
+/*required includes*/
+
+/*SST SYSTEM RESERVED SIGNALS*/
+#define SST_SIGNAL_TASKINIT		0
+/*Reserved for future use*/
+#define SST_SIGNAL_RESERVED0 	250
+#define SST_SIGNAL_RESERVED1	251	
+#define SST_SIGNAL_RESERVED2	252
+#define SST_SIGNAL_RESERVED3	253
+#define SST_SIGNAL_RESERVED4 	254
+#define SST_SIGNAL_RESERVED4 	255
+
+#endif
